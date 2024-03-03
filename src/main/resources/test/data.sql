db6a976328eb9810bf0630af5887ceba6c10ffdc
gdtknight <gdtknight@naver.com>
2024-03-03 18:44:35 +0900

    테스트용 더미 데이터 추가

---
+++ src/main/resources/test/data.sql
@@ -0,0 +1,5 @@
+insert into food_entity
+    (description, food_name, price, created_at, modified_at, created_by, last_modified_by)
+values ('너무 너무 매운 맛', '엽기떡볶이', 5000, now(), now(), 'Test User', 'Test User'),
+       ('너무 너무 구수한 맛', '차돌된장찌개', 5000, now(), now(), 'Test User', 'Test User'),
+       ('너무 너무 감칠맛', '김치찌개', 5000, now(), now(), 'Test User', 'Test User');
