60d46621c120947be27a2c79e18f72d28b63444e
gdtknight <gdtknight@naver.com>
2024-03-04 20:52:21 +0900

    FoodResponseDto 수정 - 이미지 링크 반영, Swagger Scheme 어노테이션 추가

--- src/main/java/team2/test/food/FoodResponseDto.java
+++ src/main/java/team2/test/food/FoodResponseDto.java
@@ -1,20 +1,37 @@
 package team2.test.food;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "음식 정보 응답 DTO")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @Getter
 public class FoodResponseDto {
+  @Schema(description = "음식 ID")
   private Long foodId;
+
+  @Schema(description = "음식 이름")
   private String foodName;
+
+  @Schema(description = "음식 정보")
   private String description;
+
+  @Schema(description = "음식 가격")
+  private Integer price;
+
+  @Schema(description = "음식 이미지 링크")
+  private String imageLink;
 
   public static FoodResponseDto fromEntity(FoodEntity foodEntity) {
     return new FoodResponseDto(
-        foodEntity.getFoodId(), foodEntity.getFoodName(), foodEntity.getDescription());
+        foodEntity.getFoodId(),
+        foodEntity.getFoodName(),
+        foodEntity.getDescription(),
+        foodEntity.getPrice(),
+        foodEntity.getImageLink());
   }
 }
