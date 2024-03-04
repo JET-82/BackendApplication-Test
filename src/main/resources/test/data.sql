29a5b74663becbcd5ee399c471a4a02b6082a6df
gdtknight <gdtknight@naver.com>
2024-03-04 20:45:16 +0900

    data.sql 수정 - 더미데이터 내용 수정

--- src/main/resources/test/data.sql
+++ src/main/resources/test/data.sql
@@ -1,5 +1,7 @@
 insert into food_entity
-    (description, food_name, price, created_at, modified_at, created_by, last_modified_by)
-values ('너무 너무 매운 맛', '엽기떡볶이', 5000, now(), now(), 'Test User', 'Test User'),
-       ('너무 너무 구수한 맛', '차돌된장찌개', 5000, now(), now(), 'Test User', 'Test User'),
-       ('너무 너무 감칠맛', '김치찌개', 5000, now(), now(), 'Test User', 'Test User');
+    (description, food_name, price, image_link, created_at, modified_at, created_by, last_modified_by)
+
+values ('🌶️🌶️🌶️ spicy flavor', 'Kimchi fried rice', $16.25, 'https://github.com/yeeSilver/eks/assets/48678872/e371426e-51ae-4e33-b618-226a54d6e3f9', now(), 'Test User', 'Test User'),
+       ('🌶️ spicy flavor', 'Bulgoggi bibimbap', $19.35, 'https://github.com/yeeSilver/eks/assets/48678872/0ec3c565-628d-4868-b98e-26e95b8fb582', now(), 'Test User', 'Test User'),
+       ('egg flavor', 'Kimbap', $15.00, 'https://github.com/yeeSilver/eks/assets/48678872/103d216d-bc2c-419b-be2e-a9c7da8b9888', now(), 'Test User', 'Test User'),
+       ('🌶️ spicy flavor', 'Tteokbokki and Ramen', $20.21, 'https://github.com/yeeSilver/eks/assets/48678872/b3feda5f-6323-4b5f-8c7f-ae22b424fd4f', now(), 'Test User', 'Test User');
