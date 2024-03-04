519ed7b4152dd505c444122c26b973d52f05b144
gdtknight <gdtknight@naver.com>
2024-03-04 20:55:20 +0900

    GlobalExceptionHandler 추가

---
+++ src/main/java/team2/test/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package team2.test.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import team2.test.utils.ApiResponse;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ApiResponse.Result<Object>> unknownServerError(Exception e) {
+    ApiResponse.Result<Object> apiResult =
+        ApiResponse.error(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
