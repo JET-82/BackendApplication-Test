160ca21537c0fada8db11502609dc42dbdb2f5a7
gdtknight <gdtknight@naver.com>
2024-03-04 19:09:48 +0900

    GitHub Action 테스트 - ci-food.yaml 추가

---
+++ src/main/java/team2/test/utils/ApiResponse.java
@@ -0,0 +1,36 @@
+package team2.test.utils;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse {
+  public static <T> Result<T> success() {
+    return new Result<>(HttpStatus.NO_CONTENT.value(), true, null, null);
+  }
+
+  public static <T> Result<T> success(T response) {
+    return new Result<>(HttpStatus.OK.value(), true, response, null);
+  }
+
+  public static Result<Object> error(String message, HttpStatus status) {
+    return new Result<>(status.value(), false, null, new Error(message));
+  }
+
+  @JsonInclude(JsonInclude.Include.NON_NULL)
+  @AllArgsConstructor
+  public static class Result<T> {
+    private int status;
+    private boolean success;
+    private T response;
+    private Error error;
+  }
+
+  @AllArgsConstructor
+  public static class Error {
+    private String message;
+  }
+}
