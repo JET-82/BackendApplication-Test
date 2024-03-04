6def10aebf157242f473bb179026f2f363686bae
gdtknight <gdtknight@naver.com>
2024-03-04 20:53:55 +0900

    FoodRequestDto 수정 - Swagger Scheme 어노테이션 추가

--- src/main/java/team2/test/food/FoodRequestDto.java
+++ src/main/java/team2/test/food/FoodRequestDto.java
@@ -1,16 +1,23 @@
 package team2.test.food;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "음식 정보 등록 요청 DTO")
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @NoArgsConstructor
 @Getter
 public class FoodRequestDto {
+  @Schema(description = "음식 이름")
   private String foodName;
+
+  @Schema(description = "음식 설명")
   private String description;
+
+  @Schema(description = "음식 가격")
   private Integer price;
 
   public FoodEntity toEntity() {
