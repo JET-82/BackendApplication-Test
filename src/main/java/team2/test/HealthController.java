2ed44dac2a6eaedf47f038e79533449d1e452faf
gdtknight <gdtknight@naver.com>
2024-03-05 14:21:48 +0900

    HealthCheck 컨트롤러 추가

---
+++ src/main/java/team2/test/HealthController.java
@@ -0,0 +1,13 @@
+package team2.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/")
+    public ResponseEntity health() {
+        return ResponseEntity.ok(null);
+    }
+}
