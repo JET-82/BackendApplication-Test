5363570ece9fd0a64a313214d14b93e56d97dd98
gdtknight <gdtknight@naver.com>
2024-03-13 10:53:28 +0900

    ResponseType 수정 - Swagger 태그 수정

--- src/main/java/team2/test/utils/ResponseType.java
+++ src/main/java/team2/test/utils/ResponseType.java
@@ -1,6 +1,7 @@
 package team2.test.utils;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,16 +25,27 @@ public class ResponseType {
   @JsonInclude(JsonInclude.Include.NON_NULL)
   @AllArgsConstructor
   @Getter
+  @Schema(description = "응답 객체")
   public static class Result<T> {
+    @Schema(description = "상태 코드")
     private int status;
+
+    @Schema(description = "성공 여부")
     private boolean success;
+
+    @Schema(description = "응답 데이터")
     private T response;
+
+    @Schema(description = "에러 발생시 에러 객체 포함")
     private Error error;
   }
 
+  @JsonInclude(JsonInclude.Include.NON_NULL)
   @AllArgsConstructor
   @Getter
+  @Schema(description = "Error")
   public static class Error {
+    @Schema(description = "에러 메세지")
     private String message;
   }
 }
