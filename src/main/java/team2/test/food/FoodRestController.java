9104b41d2caa5df5b0eac5246e6824fc68383b28
gdtknight <gdtknight@naver.com>
2024-03-13 10:53:04 +0900

    FoodRestController 수정 - Swagger 태그 수정

--- src/main/java/team2/test/food/FoodRestController.java
+++ src/main/java/team2/test/food/FoodRestController.java
@@ -1,78 +1,81 @@
 package team2.test.food;
 
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import team2.test.utils.ResponseType;
 
 @Slf4j
-@Tag(name = "Foods", description = "음식 정보 API")
-@Api(
-    tags = {"Foods"},
-    value = "Food Controller")
+@OpenAPIDefinition(
+    info = @Info(title = "Foods", description = "Food 도메인 API"),
+    tags = {@Tag(name = "Foods", description = "Food 도메인 태그")})
 @RestController
 @RequestMapping("/foods")
 @RequiredArgsConstructor
 public class FoodRestController {
   private final FoodService foodService;
 
-  @ApiResponses(
-      value = {
+  @Operation(
+      method = "GET",
+      summary = "음식 리스트 조회.",
+      description = "음식 리스트 반환.",
+      responses = {
         @ApiResponse(
             responseCode = "200",
             description = "리스트 조회 성공",
-            content = @Content(schema = @Schema(implementation = List.class))),
+            content =
+                @Content(
+                    array = @ArraySchema(schema = @Schema(implementation = FoodResponseDto.class))))
       })
-  @Operation(
-      summary = "음식 리스트 조회.",
-      description = "음식 리스트 반환.",
-      responses = {@ApiResponse(responseCode = "200", description = "음식 리스트 정보.")})
   @GetMapping
   public ResponseType.Result<List<FoodResponseDto>> getFoodList(HttpServletRequest request) {
 
-    String clientIp = getClientIP(request);
-
-    log.info("Client IP                : {}", clientIp);
-    log.info("Request from Remote Host : {}", request.getRemoteHost());
-    log.info("Request URI              : {}", request.getRequestURI());
-    log.info("Request Method           : {}", request.getMethod());
+    logClientInfo(request);
 
     List<FoodResponseDto> responseDtos = foodService.getFoods();
 
     return ResponseType.success(responseDtos);
   }
 
-  @ApiResponses(
-      value = {
+  @Operation(
+      method = "GET",
+      summary = "음식 상세 조회.",
+      description = "음식 상세 정보 반환.",
+      parameters = {@Parameter(in = ParameterIn.PATH)},
+      responses = {
         @ApiResponse(
             responseCode = "200",
             description = "상세 조회 성공",
             content = @Content(schema = @Schema(implementation = FoodResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "존재하지 않는 음식 ID",
+            content = @Content(schema = @Schema(implementation = ResponseType.Result.class)))
       })
-  @Operation(
-      summary = "음식 상세 조회.",
-      description = "음식 상세 정보 반환.",
-      responses = {@ApiResponse(responseCode = "200", description = "음식 상세 정보.")})
   @GetMapping("/{foodId}")
   public ResponseType.Result<FoodResponseDto> getFoodDetail(
-      @PathVariable Long foodId, HttpServletRequest request) {
+      @Parameter(
+              name = "foodId",
+              in = ParameterIn.PATH,
+              description = "상세 조회 음식 정보 ID",
+              required = true)
+          @PathVariable
+          Long foodId,
+      HttpServletRequest request) {
 
-    String clientIp = getClientIP(request);
-
-    log.info("Client IP                : {}", clientIp);
-    log.info("Request from Remote Host : {}", request.getRemoteHost());
-    log.info("Request URI              : {}", request.getRequestURI());
-    log.info("Request Method           : {}", request.getMethod());
+    logClientInfo(request, foodId);
 
     FoodResponseDto responseDto = foodService.getFoodDetail(foodId);
 
@@ -87,16 +90,62 @@ public class FoodRestController {
   public ResponseType.Result<FoodResponseDto> register(
       @RequestBody FoodRequestDto requestDto, HttpServletRequest request) {
 
+    logClientInfo(request, requestDto);
+
+    FoodResponseDto responseDto = foodService.registerFood(requestDto);
+    return ResponseType.success(responseDto);
+  }
+
+  @Operation(
+      method = "DELETE",
+      summary = "음식 정보 삭제.",
+      description = "음식 ID 에 해당하는 엔트리를 삭제한다.",
+      parameters = {@Parameter(in = ParameterIn.PATH)},
+      responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "삭제 성공",
+            content = @Content(schema = @Schema(implementation = FoodResponseDto.class))),
+        @ApiResponse(
+            responseCode = "400",
+            description = "존재하지 않는 음식 ID",
+            content = @Content(schema = @Schema(implementation = ResponseType.Result.class)))
+      })
+  @DeleteMapping("/{foodId}")
+  public ResponseType.Result<Long> deleteFood(
+      @Parameter(
+              name = "foodId",
+              in = ParameterIn.PATH,
+              description = "삭제할 음식 정보 ID",
+              required = true)
+          @PathVariable
+          Long foodId,
+      HttpServletRequest request) {
+
+    logClientInfo(request);
+
+    foodService.deleteFood(foodId);
+
+    return ResponseType.success(foodId);
+  }
+
+  private void logClientInfo(HttpServletRequest request) {
     String clientIp = getClientIP(request);
 
     log.info("Client IP                : {}", clientIp);
     log.info("Request from Remote Host : {}", request.getRemoteHost());
     log.info("Request URI              : {}", request.getRequestURI());
     log.info("Request Method           : {}", request.getMethod());
-    log.info("Request Body             : {}", requestDto);
+  }
 
-    FoodResponseDto responseDto = foodService.registerFood(requestDto);
-    return ResponseType.success(responseDto);
+  private void logClientInfo(HttpServletRequest request, Object object) {
+    String clientIp = getClientIP(request);
+
+    log.info("Client IP                : {}", clientIp);
+    log.info("Request from Remote Host : {}", request.getRemoteHost());
+    log.info("Request URI              : {}", request.getRequestURI());
+    log.info("Request Method           : {}", request.getMethod());
+    log.info("Request With             : {}", object);
   }
 
   private String getClientIP(HttpServletRequest request) {
