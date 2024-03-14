be9b7d8f7f436aa457c1e4fd530538cefd5d6515
gdtknight <gdtknight@naver.com>
2024-03-14 20:42:29 +0900

    test 이미지 링크 수정

--- src/main/resources/test/data.sql
+++ src/main/resources/test/data.sql
@@ -1,7 +1,7 @@
 insert into food_entity
     (description, food_name, price, image_link, created_at, last_modified_at, created_by, last_modified_by)
 
-values ('🌶️🌶️🌶️ spicy flavor', 'Kimchi fried rice', '16.25', 'https://github.com/yeeSilver/eks/assets/48678872/e371426e-51ae-4e33-b618-226a54d6e3f9', now(), now(), 'Test User', 'Test User'),
-       ('🌶️ spicy flavor', 'Bulgoggi bibimbap', '$19.35', 'https://github.com/yeeSilver/eks/assets/48678872/0ec3c565-628d-4868-b98e-26e95b8fb582', now(), now(),'Test User', 'Test User'),
-       ('egg flavor', 'Kimbap', '$15.00', 'https://github.com/yeeSilver/eks/assets/48678872/103d216d-bc2c-419b-be2e-a9c7da8b9888', now(), now(),'Test User', 'Test User'),
-       ('🌶️ spicy flavor', 'Tteokbokki and Ramen', '$20.21', 'https://github.com/yeeSilver/eks/assets/48678872/b3feda5f-6323-4b5f-8c7f-ae22b424fd4f', now(), now(),'Test User', 'Test User');
+values ('🌶️🌶️🌶️ spicy flavor', 'Kimchi fried rice', '16.25', 'https://ibb.co/5Rn9cJF', now(), now(), 'Test User', 'Test User'),
+       ('🌶️ spicy flavor', 'Bulgoggi bibimbap', '$19.35', 'https://ibb.co/PTtxwkt', now(), now(),'Test User', 'Test User'),
+       ('egg flavor', 'Kimbap', '$15.00', 'https://ibb.co/nkgTPHg', now(), now(),'Test User', 'Test User'),
+       ('🌶️ spicy flavor', 'Tteokbokki and Ramen', '$20.21', 'https://ibb.co/HF78wPx', now(), now(),'Test User', 'Test User');
