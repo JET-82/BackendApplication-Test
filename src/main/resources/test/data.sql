1b2295fc1404cb381d92109b600fb44d7b62dfec
gdtknight <gdtknight@naver.com>
2024-03-14 21:38:02 +0900

    테스트 데이터 진짜 수정 완료

--- src/main/resources/test/data.sql
+++ src/main/resources/test/data.sql
@@ -1,8 +1,14 @@
 insert into food_entity
-    (description, food_name, price, image_link, created_at, last_modified_at, created_by, last_modified_by)
-
-values ('🌶️🌶️🌶️ spicy flavor', 'Kimchi fried rice', '16.25', 'https://i.ibb.co/5Rn9cJF/emanuel-ekstrom-1-C-RSVhy32-Y-unsplash.jpg', now(), now(), 'Test User', 'Test User'),
-       ('🌶️ spicy flavor', 'Bulgoggi bibimbap', '$19.35', 'https://i.ibb.co/3fk1BKk/vaishnav-chogale-f-U5-WBTXa-Y-unsplash.jpg', now(), now(),'Test User', 'Test User'),
-       ('egg flavor', 'Kimbap', '$15.00', 'https://i.ibb.co/VBjdw7j/devi-puspita-amartha-yahya-Qc-Q-9-Wx-QFC0-unsplash.jpg', now(), now(),'Test User', 'Test User'),
-       ('🌶️ spicy flavor', 'Tteokbokki and Ramen', '$20.21', 'https://i.ibb.co/7YvmMpR/markus-winkler-n-E0-XNn-Gc-Q-unsplash.jpg', now(), now(),'Test User', 'Test User');
-
+(description, food_name, price, image_link, created_at, last_modified_at, created_by, last_modified_by)
+values ('🌶️🌶️🌶️ 완전 매운맛', '김치볶음밥', '₩15,000',
+        'https://i.ibb.co/5Rn9cJF/emanuel-ekstrom-1-C-RSVhy32-Y-unsplash.jpg', now(), now(), 'admin',
+        'admin'),
+       ('🌶️ 약간 매운맛', '불고기 비빔밥', '₩14,000',
+        'https://i.ibb.co/3fk1BKk/vaishnav-chogale-f-U5-WBTXa-Y-unsplash.jpg', now(), now(), 'admin',
+        'admin'),
+       ('계란이 들어갔어요', '김밥', '₩13,000',
+        'https://i.ibb.co/VBjdw7j/devi-puspita-amartha-yahya-Qc-Q-9-Wx-QFC0-unsplash.jpg', now(), now(), 'admin',
+        'admin'),
+       ('🌶️ 약간 매운맛', '떡볶이 & 라면', '₩12,000',
+        'https://i.ibb.co/7YvmMpR/markus-winkler-n-E0-XNn-Gc-Q-unsplash.jpg', now(), now(), 'admin',
+        'admin');
